4d8816cba5464a381fb03b24c7cdc763f4e46b78
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-13 16:59:11 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,9 +2,18 @@ class Model::Dog
 
   attr_accessor :name, :breed, :age
 
+  @@dogs = []
+
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+
+    @@dogs << self
   end
+
+  def self.all
+    @@dogs
+  end
+  
 end
